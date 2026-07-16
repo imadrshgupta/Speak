00ead2ade2b15799bc00420d477bae7d50ed403b
Adarsh Gupta <adarshgupta9125203@gmail.com>
2026-07-16 10:33:33 +0530

    new fixes-and features

--- src/routes/speaking.routes.ts
+++ src/routes/speaking.routes.ts
@@ -5,5 +5,8 @@ import { authenticate } from "../middleware/auth.middleware";
 const router = Router();
 
 router.post("/", authenticate, SpeakingController.createSession);
+router.get("/", authenticate, SpeakingController.history);
+router.get("/:id", authenticate, SpeakingController.details);
+router.delete("/:id", authenticate, SpeakingController.delete)
 
 export default router;
--- src/utils/password.ts
+++ src/utils/password.ts
@@ -1,12 +1,13 @@
 import bcrypt from "bcryptjs";
 
-export const hashPassword = async (password: string) => {
-    return bcrypt.hash(password, 10);
+export const hashPassword = async (password: string | number) => {
+    return bcrypt.hash(String(password), 10);
 };
 
 export const comparePassword = async (
-    password: string,
+    password: string | number,
     hashedPassword: string
 ) => {
-    return bcrypt.compare(password, hashedPassword);
+    return bcrypt.compare(String(password), hashedPassword);
 };
+
